2fdedff76986a60bd76c06048eb49fcd2a63a4e6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-05 20:01:08 -0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using Keygen
 using Test
 
 @testset "Keygen.jl" begin
-    # Write your tests here.
+
+    @testset "documenter_keygen-added" begin
+        using Example
+        documenter_keygen(user="JuliaLang", repo="git@github.com:JuliaLang/Example.jl.git")
+    end
+
+    @testset "documenter_keygen-deved" begin
+        using Pkg;
+        Pkg.develop("Example")
+        using Example
+        documenter_keygen(Example)
+    end
+
 end
